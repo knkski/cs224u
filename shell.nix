ee7ee149674a31563cda875d00c3cc48fb61ec18
Kenneth Koski <ken+git+finger@ksk.io>
2022-09-01 16:30:29 -0500

    Add nixos support via poetry2nix
    Adds pyproject.toml file with requirements mirrored from
    requirements.txt, and uses poetry2nix + shell.nix file to make it work
    for nixos.

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> { }, lib ? pkgs.lib }:
+let
+  env = pkgs.poetry2nix.mkPoetryEnv {
+    python = pkgs.python39;
+    projectDir = ./.;
+    preferWheels = true;
+    overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
+      pyzmq = super.pyzmq.overridePythonAttrs
+        (old: { buildInputs = [ pkgs.stdenv.cc.cc.lib ]; });
+
+      nbconvert = super.nbconvert.overridePythonAttrs (_: { postPatch = ""; });
+    });
+  };
+in env.env
