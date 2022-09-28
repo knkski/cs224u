48456dc9350527194b08ad30e4bded323156deed
Kenneth Koski <ken+git+finger@ksk.io>
2022-09-27 20:37:24 -0500

    Updating stuff

--- shell.nix
+++ shell.nix
@@ -9,6 +9,10 @@ let
         (old: { buildInputs = [ pkgs.stdenv.cc.cc.lib ]; });
 
       nbconvert = super.nbconvert.overridePythonAttrs (_: { postPatch = ""; });
+      astunparse = super.astunparse.overridePythonAttrs
+        (old: { buildInputs = old.buildInputs ++ [ self.wheel ]; });
+      nbdev = super.nbdev.overridePythonAttrs
+        (old: { buildInputs = old.buildInputs ++ [ self.wheel ]; });
     });
   };
 in env.env
